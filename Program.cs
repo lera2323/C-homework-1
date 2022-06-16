6a4cb88ce63fdaa296e3c39e10ed16d46ea69410
Lera2323 <vassinalera@gmail.com>
2022-06-16 14:28:15 +0300

    homework

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace S1_homework
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            /*
+            //Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+            int a = 5;
+            int b = 7;
+            if (a > b)
+            {
+                Console.WriteLine("min: " + b);
+                Console.WriteLine("max: " + a);
+            }
+            else
+            {
+                Console.WriteLine("min: " + a);
+                Console.WriteLine("max: " + b);
+            }
+            */ 
+            /*
+
+            // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+            int a = 2;
+            int b = 5;
+            int c = 7;
+            int max = a;
+
+            if (max > b)
+            {
+                max = b;
+            }
+
+            if (max < c)
+            
+            {
+                max = c;
+            }
+            Console.WriteLine("максимальное значение: " + c);
+            */
+
+            /*
+
+            // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+            int a = 4;
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine(a + " четное");
+            }
+        
+            else
+            {
+                Console.WriteLine(a + " нечетное");
+            }
+
+                */
+
+
+                //Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+                int a = 5;
+                int b = 2;
+                
+                while (b < a)
+                {
+                    Console.WriteLine(b);
+                    b += 2;
+                }
+
+
+
+    }
+}
+}
